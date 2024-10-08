000b9431e1ce9608f7a5e13e27c3728ba79ae5f6
biodaq <107210908+biodaq@users.noreply.github.com>
2024-10-08 14:37:30 +0200

    synchronized with PyPI multidaq Version 1.0.2
    Signed-off-by: biodaq <107210908+biodaq@users.noreply.github.com>

--- share/python/multidaq/__init__.py
+++ share/python/multidaq/__init__.py
@@ -6,13 +6,10 @@ Classes:
     tMsgSlave()
     tMsgMaster()
 """
-
 # flake8: noqa F401,F403
-
-__version__ = "0.0.6"
-
 minDllVersion = "1.2.0.0"
+__version__ = "1.0.2"
 
-from .hdf_stream import *
 from .multidaq import *
+from .hdf_stream import *
 from .tMsg import *
